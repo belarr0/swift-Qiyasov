095f44816bfdc8313478448073f274d52a38741a
belarr0 <belarr00ss@gmail.com>
2023-10-05 11:00:09 +0300

    first commit

---
+++ test.swift
@@ -0,0 +1,30 @@
+/*
+var emptyArray = [Int]() 
+
+var numbers = [1, 2, 3, 4, 5] 
+
+var repeatedArray = Array(repeating: 0, count: 5) 
+
+var oldArray = [3, 5, 7]
+var newArray = Array(oldArray)
+
+var newArray2 = Array(1...5)
+print(newArray2)
+*/
+
+/*
+var emptyDictionary = [String: Int]() 
+var fruits = ["apple": 5, "banana": 10, "cherry": 3] 
+
+let person = ("John", 30, "man")
+print(person.2)
+*/
+
+/*
+func Sum(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+print(Sum(3, 5))
+*/
+
